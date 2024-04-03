b20d292bedbfb5e92d0edb23de9bebce3a1b014e
Sidahmed <ncatp634@gmail.com>
2024-04-03 16:41:06 +0100

    models update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:profinder/firebase_options.dart';
+import 'package:profinder/functions/firebase_options.dart';
 import 'package:profinder/utils/theme_data.dart';
 import 'package:profinder/widgets/navigation/main_navigation_bar.dart';
 
